eb9a906d0617d93923a03f369b45047eb9a175f2
Lemon-Juiced <tbrotheawesome@gmail.com>
2023-06-08 21:58:36 -0400

    Ore Gen Made A Bit More Common

--- src/main/java/lemon_juice/better_bronze/BetterBronze.java
+++ src/main/java/lemon_juice/better_bronze/BetterBronze.java
@@ -1,20 +1,10 @@
 package lemon_juice.better_bronze;
 
-import com.mojang.logging.LogUtils;
 import lemon_juice.better_bronze.block.ModBlocks;
 import lemon_juice.better_bronze.creativetab.CreativeTab;
 import lemon_juice.better_bronze.item.ModItems;
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -22,10 +12,6 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
-import org.slf4j.Logger;
 
 @Mod(BetterBronze.MOD_ID)
 public class BetterBronze {
